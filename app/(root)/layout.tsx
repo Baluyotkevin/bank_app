f3852e37e86408bd0d79f7a8c4ccc73863bf7d03
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-06-26 14:21:01 -0700

    started and finished mobile nav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
+import Image from "next/image";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -13,7 +15,21 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
         <Sidebar user={loggedIn} />
-        {children}
+
+        <div className="flex size-full flex-col">
+          <div className="root-layout">
+            <Image 
+              src='/icons/logo.svg'
+              alt="menu icon"
+              width={30}
+              height={30}
+            />
+            <div className="">
+              <MobileNav user={loggedIn} />
+            </div>
+          </div>
+          {children}
+        </div>
     </main>
   );
 }
