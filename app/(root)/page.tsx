699486aeaeb690b4b513327fd9f0327169ef5af4
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-06-26 13:31:32 -0700

    installed qs,charts,counter and started some components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
+    const loggedIn = { firstName: "Kevin" }
+
   return (
-    <div>Home</div>
+    <section className="home">
+        <div className="home-content">
+            <header className="home-header">
+                <HeaderBox 
+                    type="greeting"
+                    title="Welcome"
+                    user={loggedIn?.firstName || 'Guest'}
+                    subtext="Access and Manage your account and transaction efficiently."
+                />
+
+                <TotalBalanceBox 
+                    accounts={[]}
+                    totalBanks={1}
+                    totalCurrentBalance={1245000.76}
+                />
+            </header>
+        </div>
+    </section>
   )
 }
 
