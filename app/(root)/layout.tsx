b3f07872ee56665b03f39383c4d02ae376a7ec1c
Kevin Baluyot <baluyotkevin33@yahoo.com>
2024-06-26 14:08:41 -0700

    started sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,18 @@
+import Sidebar from "@/components/Sidebar";
 import type { Metadata } from "next";
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = {
+    firstName: 'Kevin',
+    lastName: 'B'
+  }
+
   return (
-    <main>
-        Sidebar
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
